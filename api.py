85875f23584dc2625c381cf64231fbfe3bcbf5e2
SCcagg5 <sccagg5@gmail.com>
2023-04-30 12:21:07 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,28 +1,23 @@
-from flask import Flask, request, send_file, Response
+from bottle import route, run, request, response
 from rembg import remove
 import io
 
-app = Flask(__name__)
-
-@app.route('/detourer_image', methods=['POST'])
+@route('/detourer_image', method='POST')
 def detourer_image():
     if 'image' not in request.files:
         return {'error': 'Pas d\'image envoyée'}, 400
 
-    image = request.files['image'].read()
-    img = io.BytesIO(image)
+    image = request.files.get('image')
+    img = io.BytesIO(image.file.read())
     img_result = remove(img)
     img_result_bytes = io.BytesIO()
     img_result.save(img_result_bytes, format='PNG')
     img_result_bytes.seek(0)
 
-    def generate_response():
-        yield img_result_bytes.getvalue()
+    response.content_type = 'image/png'
+    response.set_header('Content-Disposition', 'attachment; filename=result.png')
 
-    response = Response(generate_response(), content_type='image/png')
-    response.headers['Content-Disposition'] = 'attachment; filename=result.png'
-
-    return response
+    return img_result_bytes.getvalue()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    run(host='0.0.0.0', port=8080, debug=True)
